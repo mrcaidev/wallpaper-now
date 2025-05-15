1cc6e7f9134f58a4d4d002109a06b07571e50ef1
Haiming <1411843070@qq.com>
2025-05-15 14:39:55 +0800

    fix: 新增推荐历史，5分钟内推荐过的壁纸不再重复推荐

---
+++ recommend/app/postgreDAO/recommendHistory.py
@@ -0,0 +1,47 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor, register_uuid
+from datetime import datetime
+from contextlib import contextmanager
+import logging
+import app.config.databaseConfig as DBConfig
+
+# 配置日志
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+DB_CONFIG = DBConfig.POSTGRESQL_CONFIG
+
+register_uuid()
+
+@contextmanager
+def get_db_connection():
+    conn = psycopg2.connect(**DB_CONFIG)
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+@contextmanager
+def get_db_cursor(commit=False):
+    with get_db_connection() as conn:
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        try:
+            yield cursor
+            if commit:
+                conn.commit()
+        finally:
+            cursor.close()
+async def insert_history(user_id, wallpaper_id):
+    with get_db_cursor(commit=True) as cursor:
+        cursor.execute(
+            """
+        INSERT INTO recommend_history
+        (user_id, wallpaper_id, recommendAt)
+        VALUES (%s, %s, %s)
+        """,
+            (
+                user_id,
+                wallpaper_id,
+                datetime.now()
+            )
+        )
