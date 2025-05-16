db4ababd8e0cc93cac553e13de43ac0cadd6de53
Yuwang Cai <mrcaidev@gmail.com>
2025-05-16 15:43:58 +0800

    fix: kafka bootstrapping

---
+++ init/kafka.sh
@@ -0,0 +1,4 @@
+kafka-topics.sh --bootstrap-server kafka-1:9092 --create --topic WallpaperScraped
+kafka-topics.sh --bootstrap-server kafka-1:9092 --create --topic WallpaperVectorized
+kafka-topics.sh --bootstrap-server kafka-1:9092 --create --topic InteractionCollected
+kafka-topics.sh --bootstrap-server kafka-1:9092 --create --topic UserCreated
